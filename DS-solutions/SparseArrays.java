89dff66df4bf69b5115660eb33710fe109dc8f2f
yashraj1120 <35994639+yashraj1120@users.noreply.github.com>
2019-04-02 18:55:07 +0530

    Update and rename SparseMatrix.java to SparseArrays.java

--- DS-solutions/SparseArrays.java
+++ DS-solutions/SparseArrays.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class SparseMatrix {
+public class SparseArrays {
 
     // Complete the matchingStrings function below.
     static int[] matchingStrings(String[] strings, String[] queries) {
